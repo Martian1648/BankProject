6e8d727d534c24fa086e4fd0374afa68295e299f
Martian1648 <Martian1648@outlook.com>
2024-12-28 08:43:16 -0600

    CRUD functionality for banks

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -1,36 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Services.Description;
-using BankService.Models;
+using UI.DataService;
 namespace UI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //List and Details for Models
+        #region
+        
+        public ActionResult GetAllBanks()
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                List<DataService.Bank> banks = thing.GetAllBanks().ToList();
+                return View("Banks", banks);
+
+            }
+        }
+
+        public ActionResult GetOneBank(int id)
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("Bank", thing.GetBank(id));
+            }
+        }
+
+        public ActionResult GetAllAccounts()
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("Accounts", thing.GetAllAccounts().ToList());
+            }
+        }
+
+        public ActionResult GetOneAccount(int id)
         {
             using(var thing = new DataService.DataServiceClient())
             {
-                List<DataService.Bank> banks = thing.GetAllBanks().ToList();
-                ViewData.Add("Banks", banks);
+                return View("Account", thing.GetAccount(id));
             }
-            return View();
         }
 
-        public ActionResult About()
+        public ActionResult GetAllEmployees()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("Employees", thing.GetAllEmployees().ToList());
+            }
         }
 
-        public ActionResult Contact()
+        public ActionResult GetOneEmployee(int id)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            using(var thing =new DataService.DataServiceClient())
+            {
+                return View("Employee", thing.GetEmployee(id));
+            }
         }
+
+        public ActionResult GetAllCustomers()
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("Customers", thing.GetAllCustomers().ToList());
+            }
+        }
+
+        public ActionResult GetOneCustomer(int id)
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("Customer", thing.GetCustomer(id));
+            }
+        }
+
+        public ActionResult GetAllTransactions()
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("Transactions", thing.GetAllTransactions().ToList());
+            }
+        }
+
+        public ActionResult GetOneTransactions(int id) 
+        {
+            using(var thing =new DataService.DataServiceClient())
+            {
+                return View("Transaction", thing.GetTransaction(id));
+            }
+        }
+        #endregion
+
+        //Edit for Banks
+        #region
+        public ActionResult EditBank(int id)
+        {
+            using (var thing = new DataService.DataServiceClient())
+            {
+                return View("EditBank", thing.GetBank(id));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditBank(int id, Bank bank)
+        {
+            using(var thing = new DataService.DataServiceClient())
+            {
+                thing.Update_Bank(bank);
+                return RedirectToAction("GetAllBanks");
+            }
+        }
+        #endregion
+
+        //Create & Delete for Banks
+        #region
+        public ActionResult CreateBank()
+        {
+            return View("CreateBank", new UI.DataService.Bank());
+        }
+
+        [HttpPost]
+        public ActionResult CreateBank(Bank bank)
+        {
+            using(var thing = new DataService.DataServiceClient())
+            {
+                thing.Add_Bank(bank);
+                return RedirectToAction("GetAllBanks");
+            }
+        }
+
+        public ActionResult DeleteBank(int id)
+        {
+            using(var thing = new DataService.DataServiceClient())
+            {
+                return View("DeleteBank", thing.GetBank(id));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteBank(Bank bank)
+        {
+            using(var thing  =new DataService.DataServiceClient())
+            {
+                thing.Remove_Bank(bank.Id);
+                return RedirectToAction("GetAllBanks");
+            }
+        }
+        #endregion
+
     }
 }
